d18e569632d45464b9006b9f8c9b97b370a48da1
Prangon Ghose <pghose99@gmail.com>
2017-11-06 20:29:52 -0500

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,43 +1,30 @@
-;
-; ESE_ClockCode.asm
-;
-; Created: 11/2/2017 11:47:09 AM
-; Author : Prangon Ghose0
-;
+; Created: 11/6/2017
+; Author: Prangon Ghose
 
 
-; Replace with your application code
-nop
+bender_setup:
+; Setup port c
+; Program bit 1 of portc as output, all bits are configured as inputs
+    ldi r16, 0b00000010
+	out ddrc, r16
 
-ldi r18, 0 ;remainder
-ldi r19, 0 ;quotient
-ldi r20, 5 ;dividend
-ldi r21, 0 ;divisor
+	rjmp bender_loop
 
-division:
-	cpi r21, 0 ; if the divisor is 0, go to error
-	breq error 
+bender_loop:
+; Apply voltage to bender
+	sbi portc, 1
+	nop	nop	nop	nop
+	in r17, pinc 
 
-	mov r17, r20 ;r17 is a register that is used for the branching
-	
-	sub r17, r21 ;if the divisor is greater than dividend, output the remainder and the quotient
-	brmi output
+; wait time
+	nop	nop	nop	nop nop	nop	nop	nop nop	nop	nop	nop nop	nop	nop	nop nop	nop	nop	nop nop	nop
 
-	sub r20, r21 ;subtract r21 from r20, increment the quotient (r19)
-	inc r19
+; Remove voltage from bender
+	cbi portc, 1
+	in r18, pinc
 
-	mov r17, r20
+; wait time
+	nop	nop	nop	nop nop	nop	nop	nop nop	nop	nop	nop nop	nop	nop	nop nop	nop nop nop nop nop nop	nop	nop	nop nop	nop nop nop
 
-	sub r17, r21 ;if the divisor is greater than the dividend, output the remainder and the quotient
-	brmi output
-	brpl division ;if the divisor is lower than the dividend, continue with division
-
-output:
-	mov r18, r20 ;set the value of r20 as the value of r18, the remainder
-	rjmp done ;go to done
-
-error:
-	rjmp error ;error time!
-
-done:
-	rjmp done ;we are so done :D
+; Recursion
+	rjmp bender_loop
